36fd08a41911684ac877ec2c2d01e4087ee87086
IHK_Office_Laptop <IHK_Office_Laptop>
2024-06-10 08:19:53 +0500

    improving the miniplauer dragging behaviour

--- lib/widgets/custom_miniplayer.dart
+++ lib/widgets/custom_miniplayer.dart
@@ -29,7 +29,6 @@ class _CustomMiniPlayerState extends ConsumerState<CustomMiniPlayer> {
   @override
   void initState() {
     super.initState();
-    // Set initial opacity to 0.0 when the widget is built
     _opacity = 0.0;
   }
 
@@ -41,72 +40,75 @@ class _CustomMiniPlayerState extends ConsumerState<CustomMiniPlayer> {
       maxHeight: MediaQuery.of(context).size.height,
       builder: (height, percentage) {
         if (widget.selectedVideo == null) return const SizedBox.shrink();
-        if (height <= widget._minHeight + 160) {
-          return AnimatedContainer(
-            duration: Duration(milliseconds: 200),
-            
-            color: Theme.of(context).scaffoldBackgroundColor,
+        return AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          color: Theme.of(context).scaffoldBackgroundColor,
+          child: percentage<0.6
+              ? _buildMiniPlayerContent(context)
+              : const VideoScreen(),
+        );
+      },
+    );
+  }
+
+  Widget _buildMiniPlayerContent(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        _buildMiniPlayerHeader(context),
+        const LinearProgressIndicator(
+          value: 0.22,
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMiniPlayerHeader(BuildContext context) {
+    return Row(
+      children: [
+        Image.network(
+          widget.selectedVideo!.thumbnailUrl,
+          height: widget._minHeight - 4,
+          width: 100,
+          fit: BoxFit.cover,
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  children: [
-                    Image.network(
-                      widget.selectedVideo!.thumbnailUrl,
-                      height: widget._minHeight - 4,
-                      width: 100,
-                      fit: BoxFit.cover,
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Flexible(
-                              child: Text(
-                                widget.selectedVideo!.title,
-                                style:
-                                    Theme.of(context).textTheme.labelMedium,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                            Flexible(
-                              child: Text(
-                                widget.selectedVideo!.author.username,
-                                style:
-                                    Theme.of(context).textTheme.labelMedium,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.play_arrow),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        ref.read(selectedeVideoProvider.notifier).state =
-                            null;
-                      },
-                      icon: const Icon(Icons.cancel),
-                    ),
-                  ],
+                Flexible(
+                  child: Text(
+                    widget.selectedVideo!.title,
+                    style: Theme.of(context).textTheme.labelMedium,
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
-                const LinearProgressIndicator(
-                  value: 0.22,
-                  valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+                Flexible(
+                  child: Text(
+                    widget.selectedVideo!.author.username,
+                    style: Theme.of(context).textTheme.labelMedium,
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
               ],
             ),
-          );
-        }
-        return const VideoScreen();
-      },
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.play_arrow),
+        ),
+        IconButton(
+          onPressed: () {
+            ref.read(selectedeVideoProvider.notifier).state = null;
+          },
+          icon: const Icon(Icons.cancel),
+        ),
+      ],
     );
   }
 }
